787a8fe57b93a5f5a1e5aab578abc4c3dab95c8e
c-kelly28 <ckelly@smith.edu>
2023-09-11 16:16:15 -0400

    Counted the number of characters in octocat.txt

--- octocat_download.R
+++ octocat_download.R
@@ -3,3 +3,9 @@ octocat <- readLines("http://api.github.com/octocat")
 writeLines(text = octocat, con = "octocat.txt")
 
 # can use git add in terminal to manually add a file to your repo (same as checking the staged box) 
+
+# git commit -M "Summary message" ----> terminal command to commit
+
+# SHA 1 hashes (long string of characters that is a unique identifier for a git commit)
+
+# main = master branch 
